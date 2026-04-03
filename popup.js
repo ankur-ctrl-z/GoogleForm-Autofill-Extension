6ed83434a66fb10f7ef6a0233cb10c10a31036c0
ANKUR SHARMA <ankur143513@gmail.com>
2026-04-03 14:28:02 +0530

    Final push

--- popup.js
+++ popup.js
@@ -1,8 +1,28 @@
-document.getElementById("fillBtn").addEventListener("click", async () => {
-  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+document.addEventListener("DOMContentLoaded", () => {
+  const body = document.body;
+  const toggle = document.getElementById("themeToggle");
+  const label = document.querySelector(".toggle-container span");
 
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    files: ["content.js"]
+  // load theme
+  chrome.storage.local.get("theme", (res) => {
+    const theme = res.theme || "dark";
+
+    body.classList.add(theme);
+    toggle.checked = theme === "light";
+
+    label.textContent = theme === "dark" ? "Dark Mode" : "Light Mode";
+  });
+
+  // toggle change
+  toggle.addEventListener("change", () => {
+    const newTheme = toggle.checked ? "light" : "dark";
+
+    body.classList.remove("dark", "light");
+    body.classList.add(newTheme);
+
+    chrome.storage.local.set({ theme: newTheme });
+
+    // update label (THIS WAS MISSING)
+    label.textContent = newTheme === "dark" ? "Dark Mode" : "Light Mode";
   });
 });
